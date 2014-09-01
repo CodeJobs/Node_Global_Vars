896c585e126b16e29ad6c4322459a34fff40dc3f
carlos@milkzoft.com <Carlos.X.Santana.-ND@disney.com>
2014-09-01 12:22:03 -0700

    First commit

---
+++ config.js
@@ -0,0 +1,24 @@
+var config = {
+	db: {
+		host: '127.0.0.1',
+		user: 'root',
+		password: '123456',
+		database: 'codejobs',
+		port: 3306,
+		debug: true,
+	 	//socket: '/var/run/mysqld/mysqld.sock', // For linux...
+		socket: '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock' //For mac...
+ 	},
+
+	site: {
+		url: 'http://localhost:3000',
+		title: 'Codejobs',
+		language: 'en',
+		html: {
+			engine: 'jade',
+			minify: true
+	 	}
+ 	},
+};
+ 
+module.exports = config;
